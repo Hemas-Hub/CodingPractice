e4a1d27f7241d9f37250165544140e015149dcf9
Hema <lingappanhemavathy@gmail.com>
2016-05-19 17:51:54 -0400

    Updated test cases

--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -1,80 +1,55 @@
 import org.junit.Test;
 
-/**
- * Created by hema on 5/11/16.
- */
+import static org.junit.Assert.*;
+
 public class LinkedListTest {
-    Node head = new Node();
-    LinkedList list = new LinkedList(head);
 
-    int data, pos;
+    LinkedList list = new LinkedList();
 
-  /*  @Test
-    public void insertDeleteTest(){
-       //start
-        list.insert(2,1);
-        list.insert(1, 1);
-        //end
-        list.insert(4,3);
-        list.insert(6, 4);
-        //middle
-        list.insert(3, 3);
-        list.insert(5,5);
-        list.printList();
-        System.out.println();
-        //Remove -- Start
-        list.delete(1);
-        list.delete(5);
-        list.delete(3);
-        list.printList();
-    }*/
     @Test
-    public void insertStartTest(){
-        list.insertStart(2);
+    public void test() {
+        assertTrue(list.isEmpty());
+        list.insertStart(3);
+        list.insertLast(5);
+        list.insert(4,2);
+
+        list.printList();
+        System.out.println("\n");
+
+        assertTrue(list.size() == 3);
+        assertEquals(4, list.getValue(2));
+
+        list.insert(9,10);
+
+        assertFalse(list.size() == 4);
+        assertFalse(list.getLastValue() == 9);
+
+        list.deleteLast();
+
+        list.printList();
+        System.out.println("\n");
+
+        assertFalse(list.getLastValue() == 5);
+
+        list.deleteFirst();
+
+        list.printList();
+        System.out.println("\n");
+
+        assertFalse(list.getHead() == 3);
+
         list.insertStart(1);
         list.printList();
-        System.out.println();
+        System.out.println("\n");
+
+
+        list.delete(2);
+
+        list.printList();
+        System.out.println("\n");
+
+        assertFalse(list.getValue(2) == 4);
+
     }
-    @Test
-    public void insertEndTest(){
-        list.insertLast(4);
-        list.insertLast(6);
-        list.printList();
-        System.out.println();
-      }
-    @Test
-    public void insertMiddleTest(){
-        list.insert(3, 3);
-        list.insert(5,5);
-        list.printList();
-        System.out.println();
-      }
-
-    @Test
-    public void deleteStartTest(){
-        //Remove -- Start
-        list.deleteFirst();
-        list.printList();
-        System.out.println();
-    }
-
-    @Test
-    public void deleteEndTest(){
-        list.deleteLast();
-        list.printList();
-        System.out.println();
-     }
-
-    @Test
-    public void deleteMiddleTest(){
-        list.delete(3);
-        list.printList();
-        System.out.println();
-    }
-
-    @Test
-    public void getDataTest(){
-        list.getValue(2);
-      }
 
 }
